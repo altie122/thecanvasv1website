c95876a52feceab97aed2be76db68919a64a5c14
altie122 <git@altie122.xyz>
2025-08-01 16:07:09 -0600

    fix image api

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -4,9 +4,10 @@ import { uploadThing, generateCanvasImage } from "@/lib";
 import { api } from "convex@/_generated/api";
 import { env } from "@/env";
 import { fetchQuery } from "convex/nextjs";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const img = await generateCanvasImage();
   const imageURL = "data:image/png;base64," + img.toString("base64");
-  return new Response(imageURL, { headers: { "Content-Type": "image/png" } });
+  return NextResponse.rewrite(imageURL);
 }
