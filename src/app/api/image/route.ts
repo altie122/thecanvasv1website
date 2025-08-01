4dda3a41110e2b67e4e742c99260a3947df630fc
altie122 <git@altie122.xyz>
2025-08-01 15:56:29 -0600

    add image api

---
+++ src/app/api/image/route.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { uploadThing, generateCanvasImage } from "@/lib";
+import { api } from "convex@/_generated/api";
+import { env } from "@/env";
+import { fetchQuery } from "convex/nextjs";
+
+export async function GET(request: Request) {
+  const img = await generateCanvasImage();
+  const imageURL = img.toString("base64");
+  return new Response(imageURL);
+}
